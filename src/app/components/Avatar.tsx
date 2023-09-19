2c26b280f38a2a7f14d15418ab94ca782c28a831
Tarun Kumar Singh <tarunkumarsingh98@gmail.com>
2023-09-20 02:17:19 +0530

    feat: shows login status dot

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -28,6 +28,7 @@ export default function Avatar({ image, name, isGroup }: AvatarProps) {
             font-medium
           bg-white 
           text-cyan-900 
+            relative
           "
         >
           {isGroup ? (
@@ -35,6 +36,19 @@ export default function Avatar({ image, name, isGroup }: AvatarProps) {
           ) : (
             name.slice(0, 2).toUpperCase()
           )}
+          <span
+            className="
+              absolute
+              rounded-full
+              bg-green-500
+              ring-2
+              ring-white
+              bottom-0
+              right-0
+              h-3
+              w-3
+            "
+          ></span>
         </div>
       )}
     </>
